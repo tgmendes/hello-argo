61ef06a262898915f4ade745b5ae87f751241757
Tiago Mendes <tiago.mendes@cuvva.com>
2021-08-10 15:11:54 +0100

    add flag to run service

--- main.go
+++ main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 func main() {
+	servicePtr := flag.String("service", "hello", "service to run")
+	flag.Parse()
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Hello Argo!"))
-	})
+	if *servicePtr == "hello" {
+		r.Get("/", runHello)
+	} else {
+		r.Get("/", runRaffle)
+	}
 	log.Println("Listening on port :8080")
 	_ = http.ListenAndServe(":8080", r)
+}
+
+func runHello(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte("Hello Cuvva!"))
+	return
+}
+
+func runRaffle(w http.ResponseWriter, r *http.Request) {
+	n := rand.Int()
+	_, _ = w.Write([]byte(fmt.Sprintf("your lucky number is: %d", n)))
+	return
 }
